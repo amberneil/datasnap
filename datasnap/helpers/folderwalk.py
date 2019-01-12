1af602f168f187bf73e1abd3a815fd98ac95bf3f
Neil Hansen <neil@amber.film>
2019-01-11 19:18:05 -0500

    Added folderwalk module, no fuctionality.

---
+++ datasnap/helpers/folderwalk.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+def list_child_dirs(path):
+    result = set()
+    for f in os.listdir(path):
+        fullpath = os.path.join(path, f)
+        if os.path.isdir(fullpath):
+            if os.path.realpath(fullpath) == fullpath:
+                result.add(fullpath)
+    return result
+
+def folderwalk(root, timeout=None):
+    upper_level_dirs = list_child_dirs(root)
+    key_dirs = set(i for i in upper_level_dirs)
+    if timeout:
+        condition = lambda now, timeout: now < timeout
+    else:
+        condition = lambda x, y: True
+
+    start = time.time()
+    while condition(time.time() - start, timeout):
+        this_level_dirs = set()
+        for dr in upper_level_dirs:
+            child_dirs = list_child_dirs(dr)
+            this_level_dirs.update(child_dirs)
+            key_dirs.update(child_dirs)
+            upper_level_dirs = this_level_dirs
+
+        if not this_level_dirs:
+            break
+    return key_dirs
--- datasnap/main.py
+++ datasnap/main.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 from humanfriendly import format_size
-from .helpers import (build_sets, get_stats, hash_paths, build_size_index,
+from helpers.helpers import (build_sets, get_stats, hash_paths, build_size_index,
                      select_duplicate_sizes, total_size, valid_root)
 
 
